ffe9642e7a1fe72f6b7ca398d7691663df613cfd
habrhm <m.haris.abdurrahman@gmail.com>
2022-09-20 17:32:41 +0700

    fix: package import

--- src/lib/microgen.js
+++ src/lib/microgen.js
@@ -1,4 +1,4 @@
-import { MicrogenClient } from '@microgen/sdk';
+import { MicrogenClient } from 'microgen-v3-sdk';
 
 export const microgen = new MicrogenClient({
   apiKey: import.meta.env.VITE_MICROGEN_API_KEY,
